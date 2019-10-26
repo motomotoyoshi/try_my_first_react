f16abea13134825672ee103ea95587d801626f18
motomotoyoshi <motomoto.git@gmail.com>
2019-10-27 00:43:36 +0900

    Use prop

--- react.js
+++ react.js
@@ -2,11 +2,20 @@
 
 class MyComponent extends React.Component{
     render(){
-        return React.createElement('h1', null, 'Hello World')
+        return React.createElement(
+            'input',
+            {
+                type: 'text',
+                placeholder: 'input value',
+                onKeyPress: (e) => {
+                    if(e.key === 'Enter' && e.target.value) alert(e.target.value)
+                }
+            }
+        )
     }
 }
 
 ReactDOM.render(
     React.createElement(MyComponent),
     document.getElementById('main')
-)
+);
